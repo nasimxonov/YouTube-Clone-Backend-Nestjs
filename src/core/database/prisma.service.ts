9a88bff44c3c860b5a9e6ad7084ea612720b7b01
nasimxnov <nasimkhonovs@gmail.com>
2025-06-13 14:17:56 +0500

    qanaqadir ozgarishlar qoshildi

---
+++ src/core/database/prisma.service.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    try {
+      await this.$connect();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+  async onModuleDestroy() {
+    await this.$disconnect;
+  }
+}
