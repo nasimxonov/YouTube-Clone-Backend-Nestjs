1405a4ac2f7c873fe959ad62b6a605259f2e7543
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-09-18 15:34:42 +0500

    update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import TransformInterceoptor from './common/interceptors/transform.interceptor';
@@ -11,11 +10,13 @@ import { CommentsModule } from './modules/comments/comments.module';
 import { PlaylistModule } from './modules/playlist/playlist.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { CategoryModule } from './modules/category/category.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
+    // OAuthModule,
     CoreModule,
     ChannelModule,
     VideoModule,
