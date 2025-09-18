42cc1b95dfa3388c46346f205d836e6aca6b28b1
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-09-18 15:31:20 +0500

    main.ts endded

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+
+  app.use(cookieParser());
   app.enableCors({
     origin: true,
     credentials: true,
   });
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
